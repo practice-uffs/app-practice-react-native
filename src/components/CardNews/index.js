07eccef550c575c94015a4736b41ee8a725336b9
Adriano <zorteaadriano@gmail.com>
2022-08-30 22:07:27 -0300

    feat: #2 criando card de notícias

---
+++ src/components/CardNews/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { theme } from '../../styles/theme';
+
+
+export var CardNews = (props) => {
+  return (
+    <View style={[styles.card, styles.shadow]}>
+        <Image style={styles.image}  source={props.image} />
+        <Text style={styles.title}>{props.title}</Text>
+        <Text style={styles.date}>{props.date}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: theme.colors.whiteBackground,
+    width: '100%',
+    height: 'auto',
+    minHeight: '300px',
+    margin: '0px',
+    marginBottom: '20px',
+    borderRadius: '10px',
+  },
+  image: {
+    width: '100%',
+    minHeight: '200px',
+    height: '',
+    borderTopEndRadius: '10px',
+    borderTopStartRadius: '10px'
+  },
+  title: {
+    margin: '10px',
+    marginLeft: '20px',
+    fontWeight: 'bold',
+    lineHeight: '22px',
+    fontSize:'16px',
+  },
+  date: {
+    margin: '10px',
+    marginLeft: '20px',
+    marginTop: '0px',
+  },
+  shadow: {
+    shadowColor: '#171717',
+    shadowOffset: {width: 0, height: 0},
+    shadowOpacity: 1,
+    shadowRadius: 10
+  }
+})
--- src/pages/NewsFeed/index.js
+++ src/pages/NewsFeed/index.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollViewComponent, StyleSheet, Text, View, ScrollView } from 'react-native';
 import { theme } from '../../styles/theme';
+import { CardNews } from '../../components/CardNews';
+import Routes from '../../routes';
 
 export default function NewsFeed() {
   return (
     <View style={styles.container}>
-        <Text>Aqui ficariam as notícias do PRACTICE :)</Text>
+      <ScrollView style={styles.scroll}>
+        <a href={Routes('Welcome')} style={styles.link}>
+          <CardNews image={require('../../assets/background/montanhas.png')} title="Bolsistas Bolsistas Bolsistas Bolsistas Bolsistas Bolsistas do practice atendem livre da comunidade acadêmica" date="9 de agosto de 2022" />
+        </a>
+        <CardNews onClick={'Welcome'} image={require('../../assets/background/montanhas.png')} title="Bolsistas Bolsistas Bolsistas Bolsistas Bolsistas Bolsistas do practice atendem livre da comunidade acadêmica" date="9 de agosto de 2022" />
+        <CardNews onClick={'Welcome'} image={require('../../assets/background/montanhas.png')} title="Teste de notícia" date="22 de agosto de 2022" />
+        <CardNews onClick={'Welcome'} image={require('../../assets/background/montanhas.png')} title="Nova Notícia" />
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: theme.colors.whiteBackground,
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: theme.colors.grayBackground,
+    height: '100%',
+    overflow: 'scroll',
   },
+  scroll: {
+    padding: '10px',
+    paddingTop: '20px',
+    paddingBottom: '20px',
+  },
+  link: {
+    textDecorationLine: "none",
+  }
 });
