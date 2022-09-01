9285da2d0325c24eb2d817daa5f493f90331fd10
Adriano <zorteaadriano@gmail.com>
2022-09-01 11:17:51 -0300

    buscando noticias no feed.xml

--- src/pages/NewsFeed/index.js
+++ src/pages/NewsFeed/index.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import { ScrollViewComponent, StyleSheet, Text, View, ScrollView } from 'react-native';
+import { ScrollViewComponent, StyleSheet, Text, View, ScrollView, FlatList} from 'react-native';
 import { theme } from '../../styles/theme';
 import { CardNews } from '../../components/CardNews';
 import Routes from '../../routes';
+import XMLParser from 'react-xml-parser';
+import axios from 'axios';
 
 export default function NewsFeed() {
+  const [ news, setNews ] = React.useState([]);
+  const stateFeed = [];
+
+  const getNews = () => {
+    axios.get('https:/practice.uffs.edu.br/feed.xml').then((response) => {
+      let feed = new XMLParser().parseFromString(response.data);
+      feed.children[0].children.forEach(element => {
+        if(element.name = 'item'){
+          if(element.children.length == 8){
+            stateFeed.push(element.children);
+            // setNews(stateFeed); //causa do loop 
+          }
+        }
+      });
+    });
+  }
+
+  if(!stateFeed.length){
+    getNews();
+  }
+  console.log(stateFeed);
+
   return (
     <View style={styles.container}>
       <ScrollView style={styles.scroll}>
-        <a href={Routes('Welcome')} style={styles.link}>
-          <CardNews image={require('../../assets/background/montanhas.png')} title="Bolsistas Bolsistas Bolsistas Bolsistas Bolsistas Bolsistas do practice atendem livre da comunidade acadêmica" date="9 de agosto de 2022" />
-        </a>
         <CardNews onClick={'Welcome'} image={require('../../assets/background/montanhas.png')} title="Bolsistas Bolsistas Bolsistas Bolsistas Bolsistas Bolsistas do practice atendem livre da comunidade acadêmica" date="9 de agosto de 2022" />
         <CardNews onClick={'Welcome'} image={require('../../assets/background/montanhas.png')} title="Teste de notícia" date="22 de agosto de 2022" />
         <CardNews onClick={'Welcome'} image={require('../../assets/background/montanhas.png')} title="Nova Notícia" />
