06d3e489a4363b59cc1ce78042015b54e1ad6b67
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2022-09-23 11:18:18 -0300

    checkpoint

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StyleSheet, View, Text } from 'react-native';
+import Lottie from 'lottie-react-native';
 
 const Drawer = createDrawerNavigator()
 import { AuthContext } from '../../context/auth';
@@ -9,17 +10,9 @@ export default function Services() {
   const {nome} = useContext(AuthContext);
 
   return (
-    <View style={styles.container}>
-        <Text>Aqui ficariam os exemplos de funcionalidades De serviços do mural</Text>
+    <View>
+      <Lottie source={require('./animation.json')} autoPlay loop />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "yellow",
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-});
