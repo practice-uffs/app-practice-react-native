1569f33ccb5f543eca36f9e57c01213a62de0a0d
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2022-09-26 07:59:03 -0300

    checkin

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -1,8 +1,8 @@
 import React, {useContext} from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Lottie from 'lottie-react-native';
-
+import { Text, Block, Button } from 'galio-framework'
 const Drawer = createDrawerNavigator()
 import { AuthContext } from '../../context/auth';
 
@@ -10,9 +10,34 @@ export default function Services() {
   const {nome} = useContext(AuthContext);
 
   return (
-    <View>
-      <Lottie source={require('./animation.json')} autoPlay loop />
-    </View>
+    <Block margin={20}>
+      <Lottie
+        source = {
+          require("../../assets/lottie/services.json")
+        }
+        loop
+        autoPlay
+        style = {
+          {
+            width: '75%',
+            alignSelf: 'center'
+          }
+        }
+      />
+      <Text h5>
+        Serviços
+      </Text>
+      <Block marginTop={10} marginBottom={10}>
+        <Text p size={16}>
+          O PRACTICE conta com vários serviços para ajudar você em suas atividades e projetos.
+        </Text>
+      </Block>
+      <Block center>
+        <Button uppercase flex={1} icon='pin-3' iconFamily='Galio'>
+          Solicitar Serviço
+        </Button>
+      </Block>
+    </Block>
   );
 }
 
