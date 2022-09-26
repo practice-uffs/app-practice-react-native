7c78fdec31a6add5bb5044e891f33ed15879d920
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2022-09-26 11:28:29 -0300

    checkpoint

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -1,43 +1,49 @@
 import React, {useContext} from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Linking } from 'react-native';
 import Lottie from 'lottie-react-native';
 import { Text, Block, Button } from 'galio-framework'
 const Drawer = createDrawerNavigator()
 import { AuthContext } from '../../context/auth';
+import ServicesTabs from './servicesTabs';
 
 export default function Services() {
-  const {nome} = useContext(AuthContext);
-
+  const { nome } = useContext(AuthContext);
+  
   return (
-    <Block margin={20}>
-      <Lottie
-        source = {
-          require("../../assets/lottie/services.json")
-        }
-        loop
-        autoPlay
-        style = {
-          {
-            width: '75%',
-            alignSelf: 'center'
+    <View>
+      <Block margin={20}>
+        <Lottie
+          source = {
+            require("../../assets/lottie/services.json")
           }
-        }
-      />
-      <Text h5>
-        Serviços
-      </Text>
-      <Block marginTop={10} marginBottom={10}>
-        <Text p size={16}>
-          O PRACTICE conta com vários serviços para ajudar você em suas atividades e projetos.
+          loop
+          autoPlay
+          style = {
+            {
+              width: '75%',
+              alignSelf: 'center'
+            }
+          }
+        />
+        <Text h5>
+          Serviços
         </Text>
+        <Block marginTop={10} marginBottom={10}>
+          <Text p size={16}>
+            O PRACTICE conta com vários serviços para ajudar você em suas atividades e projetos.
+          </Text>
+        </Block>
+        <Block center>
+          <Button size={'large'} uppercase round flex={1} icon='rightcircleo' iconFamily='AntDesign' onPress={() => Linking.openURL("https://practice.uffs.edu.br/mural/servicos")}>
+            Solicitar Serviço
+          </Button>
+        </Block>
       </Block>
-      <Block center>
-        <Button uppercase flex={1} icon='pin-3' iconFamily='Galio'>
-          Solicitar Serviço
-        </Button>
+      <Block>
+        <ServicesTabs/>
       </Block>
-    </Block>
+    </View>
   );
 }
 
---
+++ src/pages/Services/servicesTabs.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { View, StyleSheet, Dimensions, StatusBar } from 'react-native';
+import { TabView, SceneMap } from 'react-native-tab-view';
+
+const FirstRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
+);
+
+const SecondRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
+);
+
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+});
+
+export default function ServicesTabs() {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'first', title: 'First' },
+    { key: 'second', title: 'Second' },
+  ]);
+
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  scene: {
+    flex: 1,
+  },
+});
